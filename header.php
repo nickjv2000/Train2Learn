1fe4e16a675c5bf464ce7d6e53260a1dc224cccd
Nick <nickjv2000@hotmail.com>
2024-03-01 14:13:12 +0100

    Train2Learn.com V.0.1
    Index pagina aangemaakt met toevoegingen van header & headerbar met requires, caroussel en tekst over trainingen & workshops.
    
    Logo voor de headerbar toegevoegd in een apart mapje.
    
    headerbar gemaakt met logo, home, online cursussen, coaching, over ons & contact knoppen met doorverwijzing die nog niet actief is.
    
    header toegevoegd met info van e-mail, telefoonnummer, search bar & iconen van X (twitter), facebook en linkedin.
    
    Footer file aangemaakt, nog geen actieve code.
    
    css file toegevoegd en gelinked in alle actieve files.

---
+++ header.php
@@ -0,0 +1,26 @@
+<html>
+    <header>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/aecf3038e5.js" crossorigin="anonymous"></script>
+    </header>
+
+    <body>
+        <div class="header fixed-top">
+            <form class="form-inline my-2 my-lg-0 float-left w-100 ">
+                <i class="fa-regular fa-envelope fa-xl mr-3 ml-5 text-light"></i> 
+                <b class="pr-5 text-light">info@train2learn.nl</b>
+
+                <i class="fa-solid fa-phone fa-xl ml-5 mr-3 text-light"></i> 
+                <b class="pr-5 mr-5 text-light">0850600668</b>
+                    
+                <input class="form-control mr-sm-1 ml-5 w-50" type="text" placeholder="Zoek hier je trainingen, workshops en online cursussen" aria-label="Search">
+                <button class="btn btn-light my-2" type="submit">Zoeken</button>
+                
+                <a href=""><i class="fa-brands fa-x-twitter fa-xl pl-5 pr-3 ml-5 text-light"></i></a>
+                <a href=""><i class="fa-brands fa-facebook fa-xl pl-2 pr-3 text-light"></i></a>
+                <a href=""><i class="fa-brands fa-linkedin fa-xl pl-2 pr-3 text-light"></i></a>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ headerbar.php
@@ -0,0 +1,20 @@
+<html>
+    <header>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/aecf3038e5.js" crossorigin="anonymous"></script>
+    </header>
+    <body>
+        <nav class="headerbar navbar navbar-expand-lg navbar-dark bg-light ">
+            <img class="logo mt-5 mr-5 ml-5" src="images/logo.jpg" alt="logo">
+
+            <div class="headerbars">
+                <a href=""><h4 class="home ml-5 float-left mr-5 ml-5 mt-5"><b>Home</b></h4>
+                <a href=""><h4 class="cursussen float-left mr-5 ml-5 mt-5" ><b>Online Cursussen</b></h4>
+                <a href=""><h4 class="coaching float-left mr-5 ml-5 mt-5"><b>Coaching</b></h4>
+                <a href=""><h4 class="Over ons float-left mr-5 ml-5 mt-5"><b>Over ons</b></h4>
+                <a href=""><h4 class="contact float-left mr-5 ml-5 mt-5"><b>Contact</b></h4>
+            </div>
+        </nav>
+    </body>
+</html>
